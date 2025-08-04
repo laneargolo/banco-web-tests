241e153c670f09837c7f8fde485334e131a9eec2
laneargolo <laneargolo@gmail.com>
2025-08-03 21:59:04 -0300

    Adicionei before each para o acesso ao login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,28 @@
-describe('template spec', () => {
-  it('Login com dados válidos - Acesso com Sucesso', () => {
+describe('Login', () => {
+  beforeEach(() => {
     //Arrange 
     cy.visit('http://localhost:4000')
 
+  });
+  it('Login com dados válidos - Acesso com Sucesso', () => {
+    
     //Act 
     cy.get('#username').click().type('junior.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button','Entrar').click()
 
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+  it('Login com dados inválidos - Visualizar mensagem erro', () => {
+    
+    //Act 
+    cy.get('#username').click().type('junior.lima')
+    cy.get('#senha').click().type('456987')
+    cy.contains('button','Entrar').click()
+
+    //Assert
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
